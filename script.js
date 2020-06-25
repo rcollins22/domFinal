c4bf968b0faf60e1745f8daf1e6ebc0067d37719
Rashad Collins <rashad.collins22@gmail.com>
2020-06-25 18:10:53 -0400

    finalzed html structure

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const gameHolder = document.getElementById('Game')
 const deck =[1,2,3,4,5,6,7,8,9,'+2','+4','wild','skip','rvse']
+const pname =alert('Enter your name')
+let topCard = document.getElementById('topCard')
