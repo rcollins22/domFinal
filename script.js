afea6747f1292ef7d0faabf13e8ba83fc8394fce
Rashad Collins <rashad.collins22@gmail.com>
2020-06-26 12:04:53 -0400

    JS

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 const gameHolder = document.getElementById('Game')
-const deck =[1,2,3,4,5,6,7,8,9,'+2','+4','wild','skip','rvse']
-const pname =alert('Enter your name')
+const deck = {
+    numB:[1,2,3,4,5,6,7,8,9,'+2','+4','wild','skip','rvse'],
+    color : ['Red','Green','Blue','Yellow']
+}
+function genCard(){
+    let geCard = numB.random
+    
+}
+
+// let uPlayer.name = window.prompt('EnterName')
+let Player ={
+    myTurn: false,
+    playedCards: 0,
+    myCards:[]
+}
+let playersT =  new Map();
+Object.
+
+function player (playedCards,myTurn,myCards){
+    this.playedCards = playedCards;
+    this.myTurn = myTurn;
+    this.myCards=myCards
+}
+
+
+let cPlayer ={
+    name:'',
+    myTurn: false,
+    playedCards:0,
+    myCards:[] 
+}
+
+// let cplyr1 = 
+
 let topCard = document.getElementById('topCard')
